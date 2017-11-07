83fd924c52477e786306308ea660686f6ea26733
Benoît <ben.oeilnoir@gmail.com>
2017-11-07 22:10:21 +0100

    Rework of spline extrusion editor with add/delete options and more

--- Assets/SplineMesh/Editor/SplineExtrusionEditor.cs
+++ Assets/SplineMesh/Editor/SplineExtrusionEditor.cs
@@ -7,48 +7,98 @@ public class SplineExtrusionEditor : Editor
 {
     private const int QUAD_SIZE = 10;
     private Color CURVE_COLOR = new Color(0.8f, 0.8f, 0.8f);
+    private bool mustCreateNewNode = false;
+    private SerializedProperty textureScale;
+    private SerializedProperty vertices;
 
     private SplineExtrusion se;
     private SplineExtrusion.Vertex selectedVertex = null;
 
     private void OnEnable() {
         se = (SplineExtrusion)target;
+        textureScale = serializedObject.FindProperty("TextureScale");
+        vertices = serializedObject.FindProperty("ShapeVertices");
     }
 
     void OnSceneGUI()
     {
-        Vector3 tangent = se.spline.GetTangentAlongSpline(0);
-        Vector3 location = se.spline.GetLocationAlongSpline(0);
-        Quaternion q = CubicBezierCurve.GetRotationFromTangent(tangent);
+        Event e = Event.current;
+        if (e.type == EventType.mouseDown) {
+            Undo.RegisterCompleteObjectUndo(se, "change extruded shape");
+            // if control key pressed, we will have to create a new vertex if position is changed
+            if (e.alt) {
+                mustCreateNewNode = true;
+            }
+        }
+        if (e.type == EventType.mouseUp) {
+            mustCreateNewNode = false;
+        }
+
+        // listen for delete key
+        if (e.type == EventType.keyDown && e.keyCode == KeyCode.Delete) {
+            Undo.RegisterCompleteObjectUndo(se, "delete spline node");
+            se.ShapeVertices.Remove(selectedVertex);
+            selectedVertex = null;
+            se.GenerateMesh();
+            e.Use();
+        }
+
+
+        Vector3 splineStartTangent = se.spline.GetTangentAlongSpline(0);
+        Vector3 splineStart = se.spline.GetLocationAlongSpline(0);
+        Quaternion q = CubicBezierCurve.GetRotationFromTangent(splineStartTangent);
 
         foreach (SplineExtrusion.Vertex v in se.ShapeVertices) {
-            Vector3 vAtSplineStart = se.transform.TransformPoint(q * v.point + location);
-            Vector3 nAtSplineStart = se.transform.TransformPoint(q * (v.point + v.normal) + location);
+            // we create point and normal relative to the spline start where the shape is drawn
+            Vector3 point = se.transform.TransformPoint(q * v.point + splineStart);
+            Vector3 normal = se.transform.TransformPoint(q * (v.point + v.normal) + splineStart);
 
             if (v == selectedVertex) {
                 // draw the handles for selected vertex position and normal
-                var savedPoint = v.point;
-                var savedNormal = v.normal;
-
-                float size = HandleUtility.GetHandleSize(vAtSplineStart) * 0.3f;
+                float size = HandleUtility.GetHandleSize(point) * 0.3f;
                 float snap = 0.1f;
 
-                v.point = Quaternion.Inverse(q) * (se.transform.InverseTransformPoint(Handles.Slider2D(vAtSplineStart, tangent, Vector3.right, Vector3.up, size, Handles.CircleHandleCap, snap)) - location);
-                nAtSplineStart = se.transform.TransformPoint(q * (v.point + v.normal) + location);
-                v.normal = Quaternion.Inverse(q) * (se.transform.InverseTransformPoint(Handles.Slider2D(nAtSplineStart, tangent, Vector3.right, Vector3.up, size, Handles.CircleHandleCap, snap)) - location) - v.point;
-
-                if (savedNormal != v.normal || savedPoint != v.point) {
+                // create a handle for the vertex position
+                Vector3 movedPoint = Handles.Slider2D(point, splineStartTangent, Vector3.right, Vector3.up, size, Handles.CircleHandleCap, snap);
+                if(movedPoint != point) {
+                    // position has been moved
+                    Vector2 newVertexPoint = Quaternion.Inverse(q) * (se.transform.InverseTransformPoint(movedPoint) - splineStart);
+                    if (mustCreateNewNode) {
+                        // We must create a new node
+                        mustCreateNewNode = false;
+                        SplineExtrusion.Vertex newVertex = new SplineExtrusion.Vertex(newVertexPoint, v.normal, v.uCoord);
+                        int i = se.ShapeVertices.IndexOf(v);
+                        if(i == se.ShapeVertices.Count - 1) {
+                            se.ShapeVertices.Add(newVertex);
+                        } else {
+                            se.ShapeVertices.Insert(i + 1, newVertex);
+                        }
+                        selectedVertex = newVertex;
+                    } else {
+                        v.point = newVertexPoint;
+                        // normal must be updated if point has been moved
+                        normal = se.transform.TransformPoint(q * (v.point + v.normal) + splineStart);
+                    }
                     se.GenerateMesh();
+                } else {
+                    // vertex position handle hasn't been moved
+                    // create a handle for normal
+                    Vector3 movedNormal = Handles.Slider2D(normal, splineStartTangent, Vector3.right, Vector3.up, size, Handles.CircleHandleCap, snap);
+                    if(movedNormal != normal) {
+                        // normal has been moved
+                        v.normal = (Vector2)(Quaternion.Inverse(q) * (se.transform.InverseTransformPoint(movedNormal) - splineStart)) - v.point;
+                        se.GenerateMesh();
+                    }
                 }
 
                 Handles.BeginGUI();
-                DrawQuad(HandleUtility.WorldToGUIPoint(vAtSplineStart), CURVE_COLOR);
-                DrawQuad(HandleUtility.WorldToGUIPoint(nAtSplineStart), Color.red);
+                DrawQuad(HandleUtility.WorldToGUIPoint(point), CURVE_COLOR);
+                DrawQuad(HandleUtility.WorldToGUIPoint(normal), Color.red);
                 Handles.EndGUI();
             } else {
                 // we draw a button to allow selection of the vertex
                 Handles.BeginGUI();
-                Vector2 p = HandleUtility.WorldToGUIPoint(vAtSplineStart);
+                Vector2 p = HandleUtility.WorldToGUIPoint(point);
                 if (GUI.Button(new Rect(p - new Vector2(QUAD_SIZE / 2, QUAD_SIZE / 2), new Vector2(QUAD_SIZE, QUAD_SIZE)), GUIContent.none)) {
                     selectedVertex = v;
                 }
@@ -57,15 +107,15 @@ public class SplineExtrusionEditor : Editor
 
             // draw an arrow from the vertex location to the normal
             Handles.color = Color.red;
-            Handles.DrawLine(vAtSplineStart, nAtSplineStart);
+            Handles.DrawLine(point, normal);
 
             // draw a line between that vertex and the next one
             int index = se.ShapeVertices.IndexOf(v);
             int nextIndex = index == se.ShapeVertices.Count - 1 ? 0 : index + 1;
             SplineExtrusion.Vertex next = se.ShapeVertices[nextIndex];
             Handles.color = CURVE_COLOR;
-            Vector3 vAtSplineEnd = se.transform.TransformPoint(q * next.point + location);
-            Handles.DrawLine(vAtSplineStart, vAtSplineEnd);
+            Vector3 vAtSplineEnd = se.transform.TransformPoint(q * next.point + splineStart);
+            Handles.DrawLine(point, vAtSplineEnd);
         }
     }
 
@@ -80,4 +130,13 @@ public class SplineExtrusionEditor : Editor
     void DrawQuad(Vector2 position, Color color) {
         DrawQuad(new Rect(position - new Vector2(QUAD_SIZE / 2, QUAD_SIZE / 2), new Vector2(QUAD_SIZE, QUAD_SIZE)), color);
     }
+
+    public override void OnInspectorGUI() {
+        serializedObject.Update();
+        EditorGUILayout.HelpBox("Hold Alt and drag a vertex to create a new one.\nPress del to delete selected vertex.", MessageType.Info);
+        EditorGUILayout.PropertyField(textureScale, true);
+        EditorGUILayout.PropertyField(vertices, true);
+        serializedObject.ApplyModifiedProperties();
+    }
+
 }
