ee40acaef59bd8d84d21c2a807901bc4b8462d12
Benoît <ben.oeilnoir@gmail.com>
2019-01-29 22:49:39 +0100

    Add CameraUtility.IsOnScreen to check if a world point is in the field of view

---
+++ Assets/SplineMesh/Scripts/Utils/CameraUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace SplineMesh {
+    public static class CameraUtility {
+        public static bool IsOnScreen(Vector3 position) {
+            Vector3 onScreen = Camera.current.WorldToViewportPoint(position);
+            return onScreen.z > 0 && onScreen.x > 0 && onScreen.y > 0 && onScreen.x < 1 && onScreen.y < 1;
+        }
+    }
+}
