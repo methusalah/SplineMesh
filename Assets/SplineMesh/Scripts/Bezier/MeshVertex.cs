12e58f50c915ad6b2e0d8205278a6b4ad7b37ef5
benoit <ben.oeilnoir@gmail.com>
2019-03-22 11:58:16 +0100

    Extract of the vertex class

---
+++ Assets/SplineMesh/Scripts/Bezier/MeshVertex.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace SplineMesh {
+    [Serializable]
+    public class MeshVertex {
+        public Vector3 position;
+        public Vector3 normal;
+
+        public MeshVertex(Vector2 position, Vector2 normal) {
+            this.position = position;
+            this.normal = normal;
+        }
+    }
+}
