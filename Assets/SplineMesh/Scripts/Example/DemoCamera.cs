d139f4b2d14799d604bc4d67d86b256c7e377667
benoit <ben.oeilnoir@gmail.com>
2019-03-29 15:54:53 +0100

    added DemoCamera, to create video of the Showcase

---
+++ Assets/SplineMesh/Scripts/Example/DemoCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DemoCamera : MonoBehaviour {
+    public GameObject startExample;
+    public GameObject endExample;
+    public float speed;
+
+    private void Start() {
+        transform.position = new Vector3(
+            startExample.transform.position.x,
+            transform.position.y,
+            transform.position.z);
+    }
+
+    private void Update() {
+        transform.position -= Vector3.right * speed * Time.deltaTime;
+        if(transform.position.x < endExample.transform.position.x) {
+            transform.position = new Vector3(
+                startExample.transform.position.x,
+                transform.position.y,
+                transform.position.z);
+        }
+    }
+}
