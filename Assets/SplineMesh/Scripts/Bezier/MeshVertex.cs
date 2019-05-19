848b9cd967c4f2f85ca1bc5bbc5455c0611c0134
Benoît <ben.oeilnoir@gmail.com>
2019-05-19 22:17:09 +0200

    MeshVertex now stores the UV

--- Assets/SplineMesh/Scripts/Bezier/MeshVertex.cs
+++ Assets/SplineMesh/Scripts/Bezier/MeshVertex.cs
@@ -7,10 +7,17 @@ namespace SplineMesh {
     public class MeshVertex {
         public Vector3 position;
         public Vector3 normal;
+        public Vector2 uv;
 
-        public MeshVertex(Vector3 position, Vector3 normal) {
+        public MeshVertex(Vector3 position, Vector3 normal, Vector2 uv) {
             this.position = position;
             this.normal = normal;
+            this.uv = uv;
+        }
+
+        public MeshVertex(Vector3 position, Vector3 normal)
+            : this(position, normal, Vector2.zero)
+        {
         }
     }
 }
