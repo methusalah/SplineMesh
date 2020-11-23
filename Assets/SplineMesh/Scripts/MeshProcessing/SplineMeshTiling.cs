fe4a3f8bcd52d77d97f348a98bbd07f3c44934ca
Ben <ben.oeilnoir@gmail.com>
2020-11-23 18:10:41 +0100

    SplineMeshTiling does not generate content in the prefab editor anymore

--- Assets/SplineMesh/Scripts/MeshProcessing/SplineMeshTiling.cs
+++ Assets/SplineMesh/Scripts/MeshProcessing/SplineMeshTiling.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor.Experimental.SceneManagement;
+#endif
 
 namespace SplineMesh {
     /// <summary>
@@ -73,6 +76,10 @@ namespace SplineMesh {
         }
 
         public void CreateMeshes() {
+#if UNITY_EDITOR
+            // we don't update if we are in prefab mode
+            if (PrefabStageUtility.GetCurrentPrefabStage() != null) return;
+#endif
             var used = new List<GameObject>();
 
             if (curveSpace) {
